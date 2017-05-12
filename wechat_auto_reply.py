8f9c1513414bcd8ac78106ea73f85035a07aacd5
NUPT-Pig <nupt_b09040212@sina.com>
2017-05-12 17:21:13 +0800

    key_head 猪兔子

--- wechat_auto_reply.py
+++ wechat_auto_reply.py
@@ -39,12 +39,12 @@ def get_response(msg):
 # 这里是我们在“1. 实现微信消息的获取”中已经用到过的同样的注册方法
 @itchat.msg_register(itchat.content.TEXT, isFriendChat=True, isGroupChat=True, isMpChat=True)
 def tuling_reply(msg):
-    key_head = 'anshun:'
+    key_head = u'猪兔子'
     if key_head in msg['Text']:
         # 为了保证在图灵Key出现问题的时候仍旧可以回复，这里设置一个默认回复
         defaultReply = 'I do not know: ' + msg['Text']
         # 如果图灵Key出现问题，那么reply将会是None
-        reply = get_response(msg['Text'][len(key_head):])
+        reply = get_response(msg['Text'][len(key_head)+1:])
         print reply
         # a or b的意思是，如果a有内容，那么返回a，否则返回b
         # 有内容一般就是指非空或者非None，你可以用`if a: print('True')`来测试
